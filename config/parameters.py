cd67f6b98e364cb76f482335609e5f01680e5c01
staneee <yi.hang@live.com>
2021-01-27 16:56:55 +0800

    修改图片宽高

--- config/parameters.py
+++ config/parameters.py
@@ -1,8 +1,8 @@
 tensorLength = 248
 charLength = 62
 charNumber = 4
-ImageWidth = 120
-ImageHeight = 40
+ImageWidth = 87
+ImageHeight = 26
 
 learningRate = 3e-4  # 3e-4 #3.5e-4  # 1e-2
 totalEpoch = 600
