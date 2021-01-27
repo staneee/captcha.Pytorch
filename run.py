951c2b0eeb0aa7d4538b43fce6e56582a8dd657d
staneee <yi.hang@live.com>
2021-01-27 17:24:34 +0800

    修改run.py

--- run.py
+++ run.py
@@ -1,5 +1,3 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
 import csv
 import os
 
@@ -23,34 +21,23 @@ from model.senet import senet
 from predict import Dataset4Captcha, predict, predict_all
 from train import *
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    modelPath1 = "./weights/bnneck25_927.pth"
+    modelPath2 = "./weights/bnneck34_968.pth"
+    dataInputPath = r""
 
-    #################### 不可修改区域开始 ######################
-    # testpath = '/home/data/'						#测试集路径。包含验证码图片文件
-    # result_folder_path = '/code/result/submission.csv'	#结果输出文件路径
-    testpath = "./test"
-    result_folder_path = "./submissionaaa.csv"
-	#################### 不可修改区域结束 ######################
-    # testpath = './test/'						#测试集路径。包含验证码图片文件
-    # result_folder_path = './result/submission.csv'	#结果输出文件路径
-    print("reading start!")
-    # pic_names = [str(x) + ".jpg" for x in range(1, 5001)]
-    # pics = [imageio.imread(testpath + pic_name) for pic_name in pic_names]
-    print("reading end!")
-	### 调用自己的工程文件，并这里生成结果文件（dataframe）
-    # result = testmodel.model(testpath)
-    # print(result)
-	# # 注意路径不能更改，index需要设置为None
-    # result.to_csv(result_folder_path, index=None)
-	# ### 参考代码结束：输出标准结果文件
-    weight_path = r"./resNet_new.pth"
-    model = ResNet(ResidualBlock)
+
+    # device = torch.device('cpu')
+    
+    model = bnneck()
     model.eval()
-    model.load_model(weight_path)
-
-    if t.cuda.is_available():
-        model = model.cuda()
-
-    tdataset = Dataset4Captcha(testpath, train=False)
-    tdataloader = DataLoader(tdataset, batch_size=1, num_workers=1, shuffle=False)
-    predict(model, tdataloader, csv_file=result_folder_path)
+    
+    # model.load_model(modelPath1)
+    model.load_model(modelPath2)
+    # if t.cuda.is_available():
+    #     model = model.cuda()
+    inputDataset = Dataset4Captcha(dataInputPath, train=False)
+    inputDataLoader = DataLoader(inputDataset, batch_size=1,
+                                    shuffle=False, num_workers=1)     
+    print("start run")
+    predict(model,inputDataLoader,"result/tmp.csv")   
