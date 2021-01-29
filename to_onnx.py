4e820a44eea85b629229c2f0232cfc4f676dd2cb
staneee <yi.hang@live.com>
2021-01-29 10:00:36 +0800

    修改导出

--- to_onnx.py
+++ to_onnx.py
@@ -7,27 +7,29 @@ from model.senet import senet
 
 from config.parameters import *
 
+import torch.utils.model_zoo as model_zoo
+
 if __name__ == '__main__':
     modelPath = "./weights/bnneck34_968.pth"
     savePath = "./weights/test.onnx"
 
-    input_shape = (ImageChannel, ImageWidth, ImageHeight)
+    input_shape = (ImageChannel,  ImageHeight, ImageWidth)
 
     model = bnneck()
-    # model.eval()
-    # model.train(False)
-    model.load_model(modelPath)
+    model.load_state_dict(torch.load(modelPath, map_location='cpu'))
+    model.train(False)
 
-    dummy_input = torch.randn(1,*input_shape, device='cpu')
+    dummy_input = torch.randn(
+        1, *input_shape, device='cpu', requires_grad=True)
     input_names = ["input"]
-    output_names = ["a","b","c","d"]
+    output_names = ["a", "b", "c", "d"]
 
     torch.onnx.export(model,
-        dummy_input,
-        savePath, 
-        verbose=True, 
-        opset_version=10,
-        do_constant_folding=True,
-        input_names=input_names, 
-        output_names=output_names
-    )
+                      dummy_input,
+                      savePath,
+                      verbose=True,
+                      export_params=True,
+                      input_names=input_names,
+                      output_names=output_names,
+                      keep_initializers_as_inputs=True
+                      )
